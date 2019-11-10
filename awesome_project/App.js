47f45f07ee69f75b52ecd98e7d92ca0b7e4fd5af
NoretsMaksim <34502848+Karlsoninit@users.noreply.github.com>
2019-11-10 20:39:08 +0200

    possibility adding and delete task

--- awesome_project/App.js
+++ awesome_project/App.js
@@ -1,36 +1,56 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
 
 export default function App() {
-  const [getTask, getTaskState] = useState([]);
-  const [aloneTask, aloneTaskState] = useState("");
-  const [isOpen, isOpenState] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [createTask, setCreateTask] = useState("");
 
-  const handelSubmit = evt => {
-    aloneTaskState(evt);
+  const handelSubmit = task => {
+    setCreateTask(task);
   };
 
   const handlePress = () => {
     const task = {
-      task: aloneTask,
+      task: createTask,
       id: Math.floor(Math.random() * 100000)
     };
-    getTaskState([...getTask, task]);
-    console.log(getTask);
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = id => {
+    setTasks(getTask => {
+      return getTask.filter(task => task.id !== id);
+    });
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        onChangeText={handelSubmit}
-        placeholder="enter some task ..."
-      />
-      <Button onPress={handlePress} title="add" />
-      {getTask.map(elem => (
-        <View key={elem.id}>
-          <Text>{elem.task}</Text>
-        </View>
-      ))}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handelSubmit}
+          placeholder="enter some task ..."
+          value={createTask}
+        />
+        <Button style={styles.btn} onPress={handlePress} title="add" />
+      </View>
+      <ScrollView>
+        {tasks.map(elem => (
+          <TouchableOpacity key={elem.id} onPress={() => deleteTask(elem.id)}>
+            <View style={styles.taksListContainer} key={elem.id}>
+              <Text>{elem.task}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -39,7 +59,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center"
+  },
+  taksListContainer: {
+    width: 300,
+    backgroundColor: "grey",
+    marginBottom: 10,
+    height: 50,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "tomato"
+  },
+  inputContainer: {
+    paddingTop: 50,
+    flexDirection: "row"
+  },
+  input: {
+    borderColor: "red",
+    width: 250,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "tomato"
+  },
+  btn: {
+    height: 50
   }
 });
